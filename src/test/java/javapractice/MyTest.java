1119df619d34dc71010aee93b44e8394e0d0afb4
ChuminChu <139088490+ChuminChu@users.noreply.github.com>
2024-11-16 00:15:40 +0900

    11/16 함수실습

--- src/test/java/javapractice/MyTest.java
+++ src/test/java/javapractice/MyTest.java
@@ -8,57 +8,69 @@ public class MyTest {
     @Test
     void maxTest() {
         assertThat(Math.max(10, 7)).isEqualTo(10); //isEqualTo는 10이 아닐 시 컴퓨터가 "너 이거 틀렸어~" 라고 알려줌 즉, 테스트 해보는 역할!!
-
+        //1
         assertThat(square(5)).isEqualTo(25);
         assertThat(square(-4)).isEqualTo(16);
-
+        //2
         assertThat(getAverage(100, 100, 70)).isEqualTo(90);
         assertThat(getAverage(0, 100, 50)).isEqualTo(50);
-
+        //3
         assertThat(calculateBirthYear(1)).isEqualTo(2024);
         assertThat(calculateBirthYear(2)).isEqualTo(2023);
         assertThat(calculateBirthYear(20)).isEqualTo(2005);
-
+        //4
         assertThat(isPositive(3)).isEqualTo(true);
         assertThat(isPositive(9)).isTrue();
         assertThat(isPositive(0)).isFalse();
         assertThat(isPositive(-5)).isFalse();
-
+        //5
         assertThat(isNegative(3)).isEqualTo(false);
         assertThat(isNegative(9)).isFalse();
         assertThat(isNegative(0)).isFalse();
         assertThat(isNegative(-5)).isTrue();
-
+        //6
         assertThat(getBigger(3, 7)).isEqualTo(7);
         assertThat(getBigger(7, 3)).isEqualTo(7);
         assertThat(getBigger(-9, -1)).isEqualTo(-1);
-
+        //7
         assertThat(getBiggest(1, 2, 3)).isEqualTo(3);
         assertThat(getBiggest(3, 2, 1)).isEqualTo(3);
         assertThat(getBiggest(-3, -2, -1)).isEqualTo(-1);
         assertThat(getBiggest(-1, -2, -3)).isEqualTo(-1);
-
-        assertThat(getBiggest(1, 2, 3)).isEqualTo(3);
-        assertThat(getBiggest(3, 2, 1)).isEqualTo(3);
-        assertThat(getBiggest(-3, -2, -1)).isEqualTo(-1);
-        assertThat(getBiggest(-1, -2, -3)).isEqualTo(-1);
+        //8
+        assertThat(getSmallest(1, 2, 3)).isEqualTo(1);
+        assertThat(getSmallest(3, 2, 1)).isEqualTo(1);
+        assertThat(getSmallest(-3, -2, -1)).isEqualTo(-3);
+        assertThat(getSmallest(-1, -2, -3)).isEqualTo(-3);
+        //9
+        assertThat(abs(3)).isEqualTo(3);
+        assertThat(abs(-3)).isEqualTo(3);
+        assertThat(abs(0)).isEqualTo(0);
+        //10
+        assertThat(isEmpty("")).isTrue();
+        assertThat(isEmpty("a")).isFalse();
+        assertThat(isEmpty("    ")).isFalse();
+        //11
+        assertThat(concat("hello","world")).isEqualTo("helloworld");
+        assertThat(concat("안녕","하세요")).isEqualTo("안녕하세요");
     }
 
+    //1. 정수 하나를 입력받아 제곱한 값을 반환한다.
     int square(int n) {
         return n * n;
     }
 
-    //
+    //2. 정수 두 개를 전달받아 그 합을 반환한다.
     int getAverage(int a, int b, int c) {
         return ((a + b + c) / 3);
     }
 
-    //나이를 전달받아 출생 연도를 구해 반환한다.
+    //3. 나이를 전달받아 출생 연도를 구해 반환한다.
     int calculateBirthYear(int a) {
         return 2025 - a;
     }
 
-    //정수 하나를 전달받아 0보다 큰지 아닌지 판단 결과를 반환한다.
+    //4. 정수 하나를 전달받아 0보다 큰지 아닌지 판단 결과를 반환한다.
     boolean isPositive(int a) {
         if (a > 0) {
             return true;
@@ -66,7 +78,7 @@ public class MyTest {
         return false;
     }
 
-    //정수 하나를 전달받아 0보다 작은지 아닌지 판단 결과를 반환한다.
+    //5. 정수 하나를 전달받아 0보다 작은지 아닌지 판단 결과를 반환한다.
     boolean isNegative(int a) {
         if (a < 0) {
             return true;
@@ -74,7 +86,7 @@ public class MyTest {
         return false;
     }
 
-    //정수 2개를 전달받아 그 중 더 큰 수를 반환한다.
+    //6. 정수 2개를 전달받아 그 중 더 큰 수를 반환한다.
     int getBigger(int a, int b) {
         if (a > b) {
             return a;
@@ -82,7 +94,7 @@ public class MyTest {
         return b;
     }
 
-    //3정수를 전달받아 그 중 가장 큰 수를 반환한다.
+    //7. 3정수를 전달받아 그 중 가장 큰 수를 반환한다.
     int getBiggest(int a, int b, int c) {
         /* 1
         if(a > b && a > c ){
@@ -106,20 +118,42 @@ public class MyTest {
         return getBigger(getBigger(a,b), c);
     }
 
-    //3정수를 전달받아 그 중 가장 작은 수를 반환한다.
-    /*int min(int a, int b, int c) {
+    //8. 3정수를 전달받아 그 중 가장 작은 수를 반환한다.
 
-        }
+    //2수를 먼저 비교하는 함수식을 만든다.
+    int min(int a, int b){
+        if(a > b){
+            return b;
+        } return a;
+    }
 
-        */
+    int getSmallest(int a, int b,int c){
+        return min(min(a, b), c);
+    }
 
-    //정수 하나를 전달받아 그 절댓값을 반환한다.
+
+    //9. 정수 하나를 전달받아 그 절댓값을 반환한다.
     int abs(int a){
-        if(a < 0 ){
+        if(a < 0){
             return -a;
         } return a;
     }
 
-    //문자열 하나를 전달 받아 빈 문자열인지 판단한 결과를 반환한다.
+    //10. 문자열 하나를 전달 받아 빈 문자열인지 판단한 결과를 반환한다.
+    boolean isEmpty(String a){
+        if(a.equals("")){
+            return true;
+        } return false;
+    }
 
-    }//이거 없으면 안돌아감
+
+    //11. 두 단어를 합쳐서 출력하자
+    String concat(String s1, String s2){
+        return s1 + s2;
+    }
+
+    //'1' 또는 '3'으로 시작(남자)하면 true, 아니면 false
+    
+
+
+}
