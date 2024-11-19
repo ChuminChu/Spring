a433252d80a0947d009d884855abf87002ab3257
ChuminChu <139088490+ChuminChu@users.noreply.github.com>
2024-11-19 16:53:41 +0900

    11.19 오브젝트 테스트코드

--- src/test/java/javapractice/MyTest2.java
+++ src/test/java/javapractice/MyTest2.java
@@ -2,8 +2,8 @@ package javapractice;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.array;
 
 public class MyTest2 {
 
@@ -18,10 +18,12 @@ public class MyTest2 {
         assertThat(isEmpty("    ")).isFalse();
 
         //equals
-        assertThat(equals(4,5)).isFalse();
-        assertThat(equals(10,10)).isTrue();
+        assertThat(equals("hello")).isTrue();
+        assertThat(equals("world")).isFalse();
+
 
         //charAt
+        assertThat(charAt("hello")).isEqualTo('l');
 
         //toLowercase
         assertThat(toLowerCase("HELLO")).isEqualTo("hello");
@@ -30,22 +32,35 @@ public class MyTest2 {
         assertThat(toUpperCase("hello")).isEqualTo("HELLO");
 
         //contain
-        assertThat(contains("hello world")).isEqualTo("world");
+        assertThat(contains("hello world")).isTrue();
 
         //startWith
-        assertThat(startWith("Hello")).isEqualTo('H');
+        assertThat(startWith("Hello")).isTrue();
 
         //endsWith
-        assertThat(endsWith("Hello")).isEqualTo('o');
+        assertThat(endsWith("hello world.")).isTrue();
+        assertThat(endsWith("hello world")).isFalse();
+
 
         //repeat
-        assertThat(repeat(1)).isEqualTo(45);
+        assertThat(repeat("chu")).isEqualTo("chuchuchu");
+
 
         //indexOf()
+        assertThat(indexOf("hello")).isEqualTo(4);
 
         //concat()
         assertThat(concat("hello","world")).isEqualTo("helloworld");
         assertThat(concat("안녕","하세요")).isEqualTo("안녕하세요");
+
+        //substring
+        assertThat(substring("hello world")).isEqualTo("o wor");
+
+        //replace
+        assertThat(replace("bello world")).isEqualTo("bello world");
+
+        //trim
+        assertThat(trim("  hello  ")).isEqualTo("hello");
     }
 
 
@@ -57,61 +72,91 @@ public class MyTest2 {
     }
 
     //equals
-    boolean equals(int a, int b){
-        if(a == b){
-            return true;
-        } return false;
+    boolean equals(String a){
+        return a.equals("hello");
     }
 
-    //chatAt()
+    //chatAt() - 내가 찾는 글자가 뭐냐
+    char charAt(String str){
+        return str.charAt(3);
+    }
 
     //toLowerCase() - 소문자
     String toLowerCase(String a){
-        return a = "hello";
+        return a.toLowerCase();
     }
 
     //toUpperCase() - 대문자
     String toUpperCase(String a){
-        return a = "HELLO";
+        return a.toUpperCase();
     }
 
     //contains() - ()속의 단어를 정의된 변수에 포함하고 있냐
-    String contains(String a){
-        if(a.equals("hello world")){
-            return "world";
-        } return "hello world";
+    boolean contains(String str){
+        return str.contains("h");
     }
 
     //startWith - ()속의 것으로 시작하냐?
-    char startWith(String a){
-        if(a.equals("Hello")){
-            return 'H';
-        } return 'o';
+    boolean startWith(String str){
+        return str.startsWith("H");
     }
 
     //endsWith() - ()속의 것으로 끝나냐?
-    char endsWith(String a){
-        if(a.equals("Hello")){
-            return 'o';
-        } return 'h';
+    boolean endsWith(String str) {
+        return str.endsWith(".");
     }
 
     //repeat() - 반복
-    int repeat(int a){
-        int sum = 0;
-        for(int i = 0; i < 10; i++){
-            sum = sum + i;
-        }
-        return sum;
+    String repeat(String str){
+        return str.repeat(3);
     }
 
     //indexOf() - 한 문장속에 ()속에 있는 단어가 어디
+    int indexOf(String str){
+        return str.indexOf("o");
+    }
 
 
     //concat() - string값을 붙이는거 / 2. 두 단어를 합쳐서 출력하자
     String concat(String s1, String s2){
         return s1 + s2;
     }
+
+    //substring() - 어디서부터 어디까지 출력
+    String substring(String str){
+        return str.substring(4,9);
+    }
+
+    //replace() - 반복
+    String replace(String str){
+        return str.replace('h','b');
+    }
+
+    //trim() - 공백 삭제
+    String trim(String str){
+        return str.trim();
+    }
+
+    //String.format()
+
+
+    @Test
+    void 최대최소제한(){
+        assertThat(영과백사이(130)).isEqualTo(100);
+        assertThat(영과백사이(50)).isEqualTo(50);
+        assertThat(영과백사이(-30)).isEqualTo(0);
+    }
+    int 영과백사이(int n){
+        /*
+        int min = Math.min(n, 100);
+        return Math.max(min, 0);
+         */
+
+        return Math.max(Math.min(n, 100),0);
+
+    }
+
+
 
 
 }
