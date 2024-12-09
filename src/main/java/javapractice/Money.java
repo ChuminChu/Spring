2fc7fb43e67eaf2244939fb44d0607ebbb3e016e
ChuminChu <139088490+ChuminChu@users.noreply.github.com>
2024-12-09 17:20:52 +0900

    12/09 Money클래스의 오브젝트를 활용해서, 더하기.빼기 함수 제작, 오버라이드 활용

--- src/main/java/javapractice/Money.java
+++ src/main/java/javapractice/Money.java
@@ -1,12 +1,59 @@
 package javapractice;
 
-public class Money {
-    private int money;
+import java.util.Objects;
 
-    public Money(int money) {
-        if(money < 0){
-            //에러발생
+public class Money {
+    //값포장
+    private final int value;
+
+    public Money(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 0 미만일 수 없음");
         }
-        this.money = money;
+        this.value = value;
     }
+
+  /*
+  //Money끼리 더하는 코드
+    public Money mplus(Money amount) {
+        return new Money(amount.plus(value));
+        //amount.plus(value) = input value라고 생각하기
+
+    }
+    */
+
+    public Money plus(Money other) {
+        return new Money(this.value + other.value);
+    }
+
+    public Money minus(Money other) {
+        return new Money(this.value - other.value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return value == money.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+    /*
+    //int + int
+    public int plus(int amount) {
+        return value + amount;
+        //value 는 내가 입력받을 input타입
+    }*/
+
+    /*
+    //Money가 들어와서 int로 나가라
+    public Money minus(Money amount) {
+         return new Money(value);
+    }
+    */
+
 }
