feb2b676765bb64d85debe5276ecea72ab2fcb3c
ChuminChu <139088490+ChuminChu@users.noreply.github.com>
2024-12-10 11:17:33 +0900

    12/10 작가와 출판사에 대해 양방향 참조

--- src/main/java/javapractice/Author.java
+++ src/main/java/javapractice/Author.java
@@ -1,8 +1,13 @@
 package javapractice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Author {
     String name;
     String dataOfBitrh;
+    //작가가 쓴 책은 여러개일 수 있으니까
+    private List<Book> books = new ArrayList<>();
 
     public Author(String name, String dataOfBitrh) {
         this.name = name;
--- src/main/java/javapractice/Publisher.java
+++ src/main/java/javapractice/Publisher.java
@@ -1,9 +1,13 @@
 package javapractice;
 
 import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Publisher {
     String name;
+    //출판사가 가지고 있는 책은 여러개가 있으니까
+    private List<Book> books = new ArrayList<>();
 
     public Publisher(String name){
         this.name = name;
